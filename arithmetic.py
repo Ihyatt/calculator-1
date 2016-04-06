43f5d63eb81398aff3cf10e3d2d77c4296bdddc8
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:54:49 -0700

    Documentation added

--- arithmetic.py
+++ arithmetic.py
@@ -1,24 +1,31 @@
 def add(num1, num2):
-    return num1 + num2
-    
+""" This is function will add two numbers """
+    return num1 + num2 
 
 def subtract(num1, num2):
+""" This function will subtract two numbers """
     return num1 - num2
 
 def multiply(num1, num2):
+""" This function will multiply two numbers """
     return num1 * num2
 
 def divide(num1, num2):
+""" This funtion will divide two numbers """
     return float(num1) / num2
 
 def square(num1):
+"""This function will square a number""" 
     return num1 ** 2
 
 def cube(num1):
+""" This function will cube a number """ 
     return num1 ** 3
 
 def power(num1, num2):
+""" This function will return the power of one number by the second number """
     return num1 ** num2
 
 def mod(num1, num2):
+""" This number will return remainder of two numbers when divided """
     return num1 % num2
